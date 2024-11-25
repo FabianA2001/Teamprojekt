facb2c310d2a7773164225a8913cc16b73364df6
Fabian <>
2024-11-25 13:23:40 +0100

    option for python

--- cpp_wrapper/src/main.cpp
+++ cpp_wrapper/src/main.cpp
@@ -1,10 +1,18 @@
+
+#define python 0
+
+#if python
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#endif
 #include <vector>
 #include <utility>
+#include <iostream>
 
+#if python
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
+#endif
 
 typedef std::pair<int, int> coord;
 typedef std::vector<coord> tour;
@@ -22,11 +30,19 @@ tour ruin_and_recreate(tour points)
 {
     return points;
 }
-namespace py = pybind11;
 
-PYBIND11_MODULE(cpp_wrapper, m)
+#if python
+// namespace py = pybind11;
+// PYBIND11_MODULE(cpp_wrapper, m)
+// {
+//     m.def("two_opt", &two_opt);
+//     m.def("farthest_insertion", &farthest_insertion);
+//     m.def("ruin_and_recreate", &ruin_and_recreate);
+// }
+#endif
+
+int main()
 {
-    m.def("two_opt", &two_opt);
-    m.def("farthest_insertion", &farthest_insertion);
-    m.def("ruin_and_recreate", &ruin_and_recreate);
+    int x;
+    std::cout << "test" << std::endl;
 }
