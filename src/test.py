e70bba21ec68927433b789b423d49c25cad1f7b2
Fabian <>
2025-01-17 23:06:54 +0100

    comment

--- src/test.py
+++ src/test.py
@@ -16,18 +16,24 @@ if __name__ == "__main__":
     points = [
         Coord(6500, 1000),
         Coord(8700, 1600),
-        Coord(7900, 3000),
+        Coord(7900, 4000),
     ]
 
     img = Img([poly], points, CONST.SCREEN_WIDTH, CONST.SCREEN_HEIGHT)
     img.save("testen")
 
+    # der Letze Punkt in der Tour der nicht das Polygon schneidet
     start = points[0]
+
+    # der erste Punkt in der Tour der wieder nicht schneidet
     end = points[1]
+
     bypass_points = generate.bypass_polygon(poly, start, end)
+
+    # Tour bis inkulisve start, dann neue pyPass Punkte ,
+    # dann inklusive end bis zum ende der Tour
     points = points[:(points.index(start)+1)] + \
         bypass_points + points[points.index(end):]
 
-    print(points)
     img = Img([poly], points, CONST.SCREEN_WIDTH, CONST.SCREEN_HEIGHT)
     img.save("testen_mit_bypass")
