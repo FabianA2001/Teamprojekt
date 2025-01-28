2d64df825ca2cee368f37d3af62a640a47f3ba26
Fabian <>
2025-01-28 16:33:23 +0100

    select instence

--- src/GUI/main_gui.py
+++ src/GUI/main_gui.py
@@ -1,25 +1,51 @@
 import tkinter as tk
 import random
 
+import CONST
+
+
+class Instanze:
+    def __init__(self, name: str, poly=[], points=[], obsticales=[]) -> None:
+        self.name = name
+        self.polygone_tuple = self.get_poly_tupel(poly)
+        self.obsticales_tuple = self.get_poly_tupel(obsticales)
+        self.points_tuple = self.get_points_tuple(points)
+
+    def get_poly_tupel(self, polys: list[CONST.Polygon]):
+        result = []
+        for poly in polys:
+            poly_list = []
+            for point in poly.hull:
+                poly_list.append(tuple(point))
+            result.append(poly_list)
+        return result
+
+    def get_points_tuple(self, points):
+        result = []
+        for point in points:
+            result.append(tuple(point))
+        return result
+
 
 class GraphEditorApp:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Graph Editor")
         # Bildschirmbreite abrufen
-        # self.SCREEN_WIDTH = self.root.winfo_screenwidth()
-        # self.SCREEN_HEIGHT = self.root.winfo_screenheight()
-        self.SCREEN_WIDTH = 2000
-        self.SCREEN_HEIGHT = 1000
+        self.SCREEN_WIDTH = int(self.root.winfo_screenwidth() * 0.8)
+        self.SCREEN_HEIGHT = int(self.root.winfo_screenheight() * 0.8)
+        # self.SCREEN_WIDTH = 2000
+        # self.SCREEN_HEIGHT = 1000
 
         # Hauptvariablen
         # Liste der Polygone (jeweils als Liste von Punkten)
         self.polygons = []
         self.drawing_mode = True  # Zeichnen aktivieren/deaktivieren
         self.current_polygon = []  # Punkte des aktuellen Polygons
+        self.instes: list[Instanze] = []
 
         self.button_frame = tk.Frame(self.root)
-        self.button_frame.grid(row=0, column=0, padx=50)
+        self.button_frame.pack()
         # Buttons
         self.draw_polygon_btn = tk.Button(
             self.button_frame, text="Draw", command=self.draw)
@@ -29,43 +55,33 @@ class GraphEditorApp:
             self.button_frame, text="Remove", command=self.remove)
         self.generate_btn = tk.Button(
             self.button_frame, text="Generate", command=self.generate)
-
         self.draw_polygon_btn.pack(side='left')
         self.clear_btn.pack(side='left')
         self.remove_btn.pack(side='left')
         self.generate_btn.pack(side='left')
 
-        # Canvas für Graph
-        self.canvas = tk.Canvas(
-            # self.root, width=20, height=400, bg="white")
-            self.root, width=self.SCREEN_WIDTH*0.7, height=self.SCREEN_HEIGHT*0.9, bg="white")
-        self.canvas.grid(row=1, column=1)
-
-        # Frame für Listbox
-        self.listbox_frame = tk.Frame(
-            self.root, width=self.SCREEN_WIDTH*0.3, height=self.SCREEN_HEIGHT*0.9, bg="red")
-        self.listbox_frame.grid(row=1, column=0, sticky="nsew")
+        self.lower_frame = tk.Frame(
+            self.root)
+        self.lower_frame.pack(pady=10)
         # Listbox erstellen
-        self.listbox = tk.Listbox(self.listbox_frame, font=(
-            "Arial", 12), selectmode=tk.SINGLE)
+        self.listbox = tk.Listbox(self.lower_frame, font=(
+            "Arial", 12), selectmode=tk.SINGLE, width=30)
         # sticky sorgt dafür, dass die Listbox den Frame füllt
-        self.listbox.grid(row=0, column=0, sticky="nsew")
-
-        # Optional: Größe anpassen, damit der Frame und die Listbox den gewünschten Bereich ausfüllen
-        self.listbox_frame.grid_rowconfigure(0, weight=1)
-        self.listbox_frame.grid_columnconfigure(0, weight=1)
-
-        # Elemente zur Listbox hinzufügen
-        items = ["Apfel", "Banane", "Kirsche", "Orange",
-                 "Pfirsich", "Trauben", "Wassermelone"]
-        for item in items:
-            self.listbox.insert(tk.END, item)
-
+        self.listbox.pack(side='left', fill="y", pady=3)
         # Ereignisbindung für die Auswahl
         self.listbox.bind("<<ListboxSelect>>", self.on_item_selected)
 
-        # Canvas-Klick-Event binden
+        # Canvas für Graph
+        self.canvas = tk.Canvas(
+            self.lower_frame, height=self.SCREEN_HEIGHT-self.button_frame.winfo_height(), width=self.SCREEN_WIDTH - self.listbox.winfo_width(), bg="white")
+        self.canvas.pack(side='left')
         self.canvas.bind("<Button-1>", self.on_canvas_click)
+
+        # # Elemente zur Listbox hinzufügen
+        # items = ["Apfel", "Banane", "Kirsche", "Orange",
+        #          "Pfirsich", "Trauben", "Wassermelone"]
+        # for item in items:
+        #     self.listbox.insert(tk.END, item)
 
     def draw_polygon(self, poly):
         self.canvas.create_polygon(
@@ -101,7 +117,20 @@ class GraphEditorApp:
         self.draw_polygon_btn.config(state="disabled")
         self.remove_btn.config(state="disabled")
         self.clear_btn.config(state="disabled")
-        self.show_popup("gen", "generate")
+        self.canvas.delete("all")
+
+        polys_poly = []
+        for poly in self.polygons:
+            poly_coord: list[CONST.Coord] = []
+            for point in poly:
+                poly_coord.append(CONST.Coord(point[0], point[1]))
+            polys_poly.append(CONST.Polygon(poly_coord))
+        inst = Instanze("Blank", polys_poly)
+        self.instes.append(inst)
+        self.listbox.insert(tk.END, self.instes[0].name)
+
+        self.instes.append(Instanze("zwei", poly=polys_poly[:2]))
+        self.listbox.insert(tk.END, self.instes[1].name)
 
     def remove(self):
         if len(self.polygons) == 0:
@@ -116,10 +145,11 @@ class GraphEditorApp:
         # Ausgewählte Elemente abrufen
         selected_indices = self.listbox.curselection()  # Index der ausgewählten Elemente
         if selected_indices:
+            inst = self.instes[selected_indices[0]]
             # Text des ausgewählten Elements
-            selected_item = self.listbox.get(selected_indices[0])
-            self.show_popup("Element ausgewählt", f"Du hast '{
-                            selected_item}' ausgewählt!")
+            self.canvas.delete("all")
+            for poly in inst.polygone_tuple:
+                self.draw_polygon(poly)
 
     # Function to show the popup
     def show_popup(self, titel, text):
